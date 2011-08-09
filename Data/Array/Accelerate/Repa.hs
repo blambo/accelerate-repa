95ec58e4e00c60fc4a30628be4b99823ecd730d2
Ben Lambert-Smith <blambo+github@gmail.com>
2011-08-09 22:43:03 +1000

    Initialised cabal infrastructure

---
+++ Data/Array/Accelerate/Repa.hs
@@ -0,0 +1,12 @@
+module Data.Array.Accelerate.Repa
+   ( Arrays
+   , run
+   )
+   where
+
+import Data.Array.Accelerate.AST    (Arrays(..))
+import Data.Array.Accelerate.Smart  (Acc)
+
+-- | Used to compile and run an embedded array program using the Repa backend
+run :: Arrays a => Acc a -> a
+run _ = error "Undefined run program"
