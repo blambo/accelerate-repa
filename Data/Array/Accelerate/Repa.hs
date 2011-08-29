252c2176a8f4428e91f994adf21e0c4144edad3b
Ben Lambert <blambo+github@gmail.com>
2011-08-29 16:51:40 +1000

    Small change to Repa.hs

--- Data/Array/Accelerate/Repa.hs
+++ Data/Array/Accelerate/Repa.hs
@@ -4,8 +4,8 @@ module Data.Array.Accelerate.Repa
    )
    where
 
-import Data.Array.Accelerate.AST    (Arrays(..))
-import Data.Array.Accelerate.Smart  (Acc)
+import Data.Array.Accelerate.AST
+--import Data.Array.Accelerate.Smart  (Acc)
 
 -- | Used to compile and run an embedded array program using the Repa backend
 run :: Arrays a => Acc a -> a
