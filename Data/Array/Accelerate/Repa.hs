e4a999580b72136775fa590bbfffb689f330b9e0
Ben Lambert-Smith <blambo+github@gmail.com>
2011-08-30 10:25:05 +1000

    Very basic traversal implemented over AST

--- Data/Array/Accelerate/Repa.hs
+++ Data/Array/Accelerate/Repa.hs
@@ -20,73 +20,194 @@ import qualified Data.Array.Accelerate.Smart as Smart
 --import Data.Array.Accelerate.Repa.Convert (accToRepa)
 
 -- | Used to compile and run an embedded array program using the Repa backend
-run :: Arrays a => Smart.Acc a -> a
+run :: Arrays a => Smart.Acc a -> IO ()
 run = evalAcc . Smart.convertAcc
 
-evalAcc :: Acc a -> a
+evalAcc :: Acc a -> IO ()
 evalAcc acc = evalOpenAcc acc Empty
 
-evalOpenAcc :: OpenAcc aenv a -> Val aenv -> a
+-- | Unpacks AST by removing 'OpenAcc' shell
+evalOpenAcc :: OpenAcc aenv a -> Val aenv -> IO ()
 evalOpenAcc (OpenAcc acc) = evalPreOpenAcc acc
 
-evalPreOpenAcc :: PreOpenAcc OpenAcc aenv a -> Val aenv -> a
+-- | Traverses over AST
+evalPreOpenAcc :: PreOpenAcc OpenAcc aenv a -> Val aenv -> IO ()
 
-evalPreOpenAcc (Let _acc1 _acc2) _aenv = error "Let"
+evalPreOpenAcc (Let acc1 acc2) aenv
+ = do
+      print "Let {"
+      evalOpenAcc acc1 aenv
+      --evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Let2 _acc1 _acc2) _aenv = error "Let2"
+evalPreOpenAcc (Let2 acc1 acc2) aenv
+ = do
+      print "Let2 {"
+      evalOpenAcc acc1 aenv
+      --evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (PairArrays _acc1 _acc2) _aenv = error "PairArrays"
+evalPreOpenAcc (PairArrays acc1 acc2) aenv
+ = do
+      print "PairArrays {"
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Avar _idx) _aenv = error "Avar"
+evalPreOpenAcc (Avar _idx) _aenv
+ = do
+      print "Avar {"
+      print "}"
 
-evalPreOpenAcc (Apply (Alam (Abody _funAcc)) _acc) _aenv = error "Apply"
+evalPreOpenAcc (Apply (Alam (Abody funAcc)) acc) aenv
+ = do
+      print "Apply {"
+      evalOpenAcc acc aenv
+      --evalOpenAcc funAcc aenv
+      print "}"
 evalPreOpenAcc (Apply _afun _acc) _aenv
    = error "GHC pattern matching does not detect that this case is impossible"
 
-evalPreOpenAcc (Acond _cond _acc1 _acc2) _aenv = error "Acond"
+evalPreOpenAcc (Acond cond acc1 acc2) aenv
+ = do
+      print "Acond {"
+      --evalOpenAcc cond aenv
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Use _arr) _aenv = error "Use"
+evalPreOpenAcc (Use _arr) _aenv
+ = do
+      print "Use {"
+      print "}"
 
-evalPreOpenAcc (Unit _e) _aenv = error "Unit"
+evalPreOpenAcc (Unit _e) _aenv
+ = do
+      print "Unit {"
+      print "}"
 
-evalPreOpenAcc (Reshape _e _acc) _aenv = error "Reshape"
+evalPreOpenAcc (Reshape _e acc) aenv
+ = do
+      print "Reshape {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Generate _sh _f) _aenv = error "Generate"
+evalPreOpenAcc (Generate _sh _f) _aenv
+ = do
+      print "Generate {"
+      print "}"
 
-evalPreOpenAcc (Replicate _sliceIndex _slix _acc) _aenv = error "Replicate"
+evalPreOpenAcc (Replicate _sliceIndex _slix acc) aenv
+ = do
+      print "Replicate {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Index _sliceIndex _acc _slix) _aenv = error "Index"
+evalPreOpenAcc (Index _sliceIndex acc _slix) aenv
+ = do
+      print "Index {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Map _f _acc) _aenv = error "Map"
+evalPreOpenAcc (Map _f acc) aenv
+ = do
+      print "Map {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (ZipWith _f _acc1 _acc2) _aenv = error "ZipWith"
+evalPreOpenAcc (ZipWith _f acc1 acc2) aenv
+ = do
+      print "ZipWith {"
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Fold _f _e _acc) _aenv = error "Fold"
+evalPreOpenAcc (Fold _f _e acc) aenv
+ = do
+      print "Fold {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Fold1 _f _acc) _aenv = error "Fold1"
+evalPreOpenAcc (Fold1 _f acc) aenv
+ = do
+      print "Fold1 {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (FoldSeg _f _e _acc1 _acc2) _aenv = error "FoldSeg"
+evalPreOpenAcc (FoldSeg _f _e acc1 acc2) aenv
+ = do
+      print "FoldSeg {"
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Fold1Seg _f _acc1 _acc2) _aenv = error "Fold1Seg"
+evalPreOpenAcc (Fold1Seg _f acc1 acc2) aenv
+ = do
+      print "Fold1Seg {"
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
-evalPreOpenAcc (Scanl _f _e _acc) _aenv = error "Scanl"
+evalPreOpenAcc (Scanl _f _e acc) aenv
+ = do
+      print "Scanl {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Scanl' _f _e _acc) _aenv = error "Scanl'"
+evalPreOpenAcc (Scanl' _f _e acc) aenv
+ = do
+      print "Scanl' {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Scanl1 _f _acc) _aenv = error "Scanl1"
+evalPreOpenAcc (Scanl1 _f acc) aenv
+ = do
+      print "Scanl1 {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Scanr _f _e _acc) _aenv = error "Scanr"
+evalPreOpenAcc (Scanr _f _e acc) aenv
+ = do
+      print "Scanr {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Scanr' _f _e _acc) _aenv = error "Scanr'"
+evalPreOpenAcc (Scanr' _f _e acc) aenv
+ = do
+      print "Scanr' {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Scanr1 _f _acc) _aenv = error "Scanr1"
+evalPreOpenAcc (Scanr1 _f acc) aenv
+ = do
+      print "Scanr1 {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Permute _f _dftAcc _p _acc) _aenv = error "Permute"
+evalPreOpenAcc (Permute _f dftAcc _p acc) aenv
+ = do
+      print "Permute {"
+      evalOpenAcc dftAcc aenv
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Backpermute _e _p _acc) _aenv = error "Backpermute"
+evalPreOpenAcc (Backpermute _e _p acc) aenv
+ = do
+      print "Backpermute {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Stencil _sten _bndy _acc) _aenv = error "Stencil"
+evalPreOpenAcc (Stencil _sten _bndy acc) aenv
+ = do
+      print "Stencil {"
+      evalOpenAcc acc aenv
+      print "}"
 
-evalPreOpenAcc (Stencil2 _sten _bndy1 _acc1 _bndy2 _acc2) aenv = error "Stencil2"
+evalPreOpenAcc (Stencil2 _sten _bndy1 acc1 _bndy2 acc2) aenv
+ = do
+      print "Stencil2 {"
+      evalOpenAcc acc1 aenv
+      evalOpenAcc acc2 aenv
+      print "}"
 
 evalPreOpenAcc _ _ = error "Not yet implemented"
