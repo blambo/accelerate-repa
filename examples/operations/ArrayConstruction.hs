f1aaeec46b5e14d0e462429b4f8bd1129afcaabb
Ben Lambert <blambo+github@gmail.com>
2011-09-07 18:37:54 +1000

    Expanding on test cases

--- examples/operations/ArrayConstruction.hs
+++ examples/operations/ArrayConstruction.hs
@@ -7,12 +7,33 @@ import Prelude hiding (replicate)
 
 main :: IO ()
 main = do
-   print $ Repa.run $ unit $ constant (3 :: Int)
+   -- unit
+   putStrLn $ Repa.run $ unit $ constant (3 :: Int)
+
+   -- use
    -- print $ Repa.run $ use  $ constant (3 :: Int)
-   print "TODO: 'use'"
-   print $ Repa.run $ generate (index2 (index1 3) 3) (\x -> constant (1 :: Int))
+   putStrLn "TODO: 'use'"
+
+   -- generate
+   putStrLn $ Repa.run $ generate (index2 (index1 3) 3) (\x -> constant (1 :: Int))
+
+   -- replicate
+   putStrLn "TODO: 'replicate'"
 --   print $ Repa.run $ replicate (Z :. All :. 3)
 --                    $ generate  (index1 3)      (\x -> constant 1)
+
+   -- pairA
+   putStrLn $ Repa.run $ pairA scalarArr scalarArr
+
+   -- fstA
+   putStrLn $ Repa.run $ fstA $ pairA scalarArr scalarArr
+
+   -- sndA
+   putStrLn $ Repa.run $ sndA $ pairA scalarArr scalarArr
+
+
+scalarArr :: Acc (Array Z Int)
+scalarArr = generate index0 (\x -> constant 1)
 
 index2 :: Exp (Z:.Int) -> Int -> Exp (Z:.Int:.Int)
 index2 sh x = lift ( sh :. x )
