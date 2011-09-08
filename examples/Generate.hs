bba85c86d664b1c27dba74e1f5d5888ea5ce26d9
Ben Lambert <blambo+github@gmail.com>
2011-09-08 17:11:06 +1000

    wibble in Generate test

--- examples/Generate.hs
+++ examples/Generate.hs
@@ -2,4 +2,4 @@ import Data.Array.Accelerate
 import Data.Array.Accelerate.Repa as Repa
 
 main :: IO ()
-main = print $ Repa.run $ generate index0 (\x -> constant (1 :: Int))
+main = putStrLn $ Repa.run $ generate index0 (\x -> constant (1 :: Int))
