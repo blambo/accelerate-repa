06042ec5aa4f237f0f3c13a89d361363ae0b85ad
Ben Lambert <blambo+github@gmail.com>
2011-09-09 16:11:24 +1000

    Expanded some test cases in ArrayConstruction

--- examples/operations/ArrayConstruction.hs
+++ examples/operations/ArrayConstruction.hs
@@ -8,32 +8,45 @@ import Prelude hiding (replicate)
 main :: IO ()
 main = do
    -- unit
+   putStrLn "---UNIT---"
    putStrLn $ Repa.run $ unit $ constant (3 :: Int)
 
    -- use
+   putStrLn "---USE---"
    -- print $ Repa.run $ use  $ constant (3 :: Int)
    putStrLn "TODO: 'use'"
 
    -- generate
+   putStrLn "---GENERATE---"
    putStrLn $ Repa.run $ generate (index2 (index1 3) 3) (\x -> constant (1 :: Int))
 
    -- replicate
+   putStrLn "---REPLICATE---"
    putStrLn "TODO: 'replicate'"
 --   print $ Repa.run $ replicate (Z :. All :. 3)
 --                    $ generate  (index1 3)      (\x -> constant 1)
 
    -- pairA
-   putStrLn $ Repa.run $ pairA scalarArr scalarArr
+   putStrLn "---PAIRA---"
+   putStrLn $ Repa.run $ pairA testArr testArr
+   putStrLn $ Repa.run $ pairA (scalarArr 1) (scalarArr 1)
+   putStrLn $ Repa.run $ pairA (scalarArr 2) (scalarArr 3)
 
    -- fstA
-   putStrLn $ Repa.run $ fstA $ pairA scalarArr scalarArr
+   putStrLn "---FSTA---"
+   putStrLn $ Repa.run $ fstA $ pairA testArr testArr
+   putStrLn $ Repa.run $ fstA $ pairA (scalarArr 4) (scalarArr 5)
 
    -- sndA
-   putStrLn $ Repa.run $ sndA $ pairA scalarArr scalarArr
+   putStrLn "---SNDA---"
+   putStrLn $ Repa.run $ sndA $ pairA testArr testArr
+   putStrLn $ Repa.run $ sndA $ pairA (scalarArr 6) (scalarArr 7)
 
+testArr :: Acc (Array Z Int)
+testArr = generate index0 (\x -> constant 0)
 
-scalarArr :: Acc (Array Z Int)
-scalarArr = generate index0 (\x -> constant 1)
+scalarArr :: Int -> Acc (Array Z Int)
+scalarArr c = generate index0 (\x -> constant c)
 
 index2 :: Exp (Z:.Int) -> Int -> Exp (Z:.Int:.Int)
 index2 sh x = lift ( sh :. x )
