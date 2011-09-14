514612abf3db3a46e578fe89ae10ba497eba1da8
Ben Lambert <blambo+github@gmail.com>
2011-09-14 14:12:38 +1000

    Added reshape test

--- examples/operations/03_ShapeManipulation.hs
+++ examples/operations/03_ShapeManipulation.hs
@@ -4,7 +4,10 @@ import Data.Array.Accelerate
 import Data.Array.Accelerate.Repa as Repa
 
 main :: IO ()
-main = putStrLn $ Repa.run $ reshape (index2 2 10) $ generate (index2 4 5) $ (\x -> constant (3::Int))
+main = do
+   putStrLn "---RESHAPE---"
+   putStrLn $ Repa.run $ reshape (index2 2 10) $ generate (index2 4 5) $ (\x -> constant (3::Int))
+   putStrLn $ Repa.run $ reshape (index2 4 5) $ generate (index2 4 5) $ (\x -> constant (3::Int))
 
 index2 :: Int -> Int -> Exp (Z:.Int:.Int)
 index2 x y = lift $ (lift (lift Z):.y):.x
