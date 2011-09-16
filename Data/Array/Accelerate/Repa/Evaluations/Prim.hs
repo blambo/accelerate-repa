e25ff6cedc1e2d96fe4b2da7917c1e8fd359ad27
Ben Lambert <blambo+github@gmail.com>
2011-09-16 16:04:22 +1000

    Fleshed out Prim evaluations

--- Data/Array/Accelerate/Repa/Evaluations/Prim.hs
+++ Data/Array/Accelerate/Repa/Evaluations/Prim.hs
@@ -19,83 +19,57 @@ evalPrimConst _ = "PrimConst"
 
 evalPrim :: PrimFun p -> String -> String
 
-evalPrim (PrimAdd ty) s = "( (fst " ++ s ++ ") + (snd " ++ s ++ ") )"
-
-evalPrim (PrimSub ty) s = "sub"
-
-evalPrim (PrimMul ty) s = "mul"
-
-evalPrim (PrimNeg ty) s = "neg"
-
-evalPrim (PrimAbs ty) s = "abs"
-
-evalPrim (PrimSig ty) s = "sig"
-
-evalPrim (PrimQuot ty) s = "quot"
-
-evalPrim (PrimRem ty) s = "rem"
-
-evalPrim (PrimIDiv ty) s = "idiv"
-
-evalPrim (PrimMod ty) s = "mod"
-
-evalPrim (PrimBAnd ty) s = "bAnd"
-
-evalPrim (PrimBOr ty) s = "bOr"
-
-evalPrim (PrimBXor ty) s = "bXor"
-
-evalPrim (PrimBNot ty) s = "bNot"
-
-evalPrim (PrimBShiftL ty) s = "bShiftl"
-
-evalPrim (PrimBShiftR ty) s = "bShiftr"
-
-evalPrim (PrimBRotateL ty) s = "bRotatel"
-
-evalPrim (PrimBRotateR ty) s = "bRotater"
-
-evalPrim (PrimFDiv ty) s = "fdiv"
-
-evalPrim (PrimRecip ty) s = "recip"
-
-evalPrim (PrimSin ty) s = "sin"
-
-evalPrim (PrimCos ty) s = "cos"
-
-evalPrim (PrimTan ty) s = "tan"
-
-evalPrim (PrimAsin ty) s = "asin"
-
-evalPrim (PrimAcos ty) s = "acos"
-
-evalPrim (PrimAtan ty) s = "atan"
-
-evalPrim (PrimAsinh ty) s = "asinh"
-evalPrim (PrimAcosh ty) s = "acosh"
-evalPrim (PrimAtanh ty) s = "atanh"
-evalPrim (PrimExpFloating ty) s = "expfloating"
-evalPrim (PrimSqrt ty) s = "sqrt"
-evalPrim (PrimLog ty) s = "log"
-evalPrim (PrimFPow ty) s = "fpow"
-evalPrim (PrimLogBase ty) s = "logbase"
-evalPrim (PrimTruncate ta tb) s = "truncate"
-evalPrim (PrimRound ta tb) s = "round"
-evalPrim (PrimFloor ta tb) s = "floor"
-evalPrim (PrimCeiling ta tb) s = "ceiling"
-evalPrim (PrimAtan2 ty) s = "atan2"
-evalPrim (PrimLt ty) s = "lt"
-evalPrim (PrimGt ty) s = "gt"
-evalPrim (PrimLtEq ty) s = "lteq"
-evalPrim (PrimGtEq ty) s = "gteq"
-evalPrim (PrimEq ty) s = "eq"
-evalPrim (PrimNEq ty) s = "neq"
-evalPrim (PrimMax ty) s = "max"
-evalPrim (PrimMin ty) s = "min"
-evalPrim (PrimLAnd) s = "lAnd"
-evalPrim (PrimLOr) s = "lOr"
-evalPrim (PrimLNot) s = "lNot"
-evalPrim (PrimOrd) s = "ord"
-evalPrim (PrimChr) s = "chr"
-evalPrim (PrimBoolToInt) s = "booltoint"
-evalPrim (PrimFromIntegral ta tb) s = "fromIntegral"
+evalPrim (PrimAdd ty) s         = "( (fst " ++ s ++ ") + (snd " ++ s ++ ") )"
+evalPrim (PrimSub ty) s         = "( (fst " ++ s ++ ") - (snd " ++ s ++ ") )"
+evalPrim (PrimMul ty) s         = "( (fst " ++ s ++ ") * (snd " ++ s ++ ") )"
+evalPrim (PrimNeg ty) s         = "(negate " ++ s ++ ")"
+evalPrim (PrimAbs ty) s         = "(abs " ++ s ++ ")"
+evalPrim (PrimSig ty) s         = "(signum " ++ s ++ ")"
+evalPrim (PrimQuot ty) s        = "((fst " ++ s ++ ") `quot` (snd " ++ s ++ "))"
+evalPrim (PrimRem ty) s         = "( (fst " ++ s ++ ") `rem` (snd " ++ s ++ "))"
+evalPrim (PrimIDiv ty) s        = "( (fst " ++ s ++ ") `div` (snd " ++ s ++ "))"
+evalPrim (PrimMod ty) s         = "( (fst " ++ s ++ ") `mod` (snd " ++ s ++ "))"
+evalPrim (PrimBAnd ty) s        = "( (fst " ++ s ++ ") .&. (snd " ++ s ++ ") )"
+evalPrim (PrimBOr ty) s         = "( (fst " ++ s ++ ") .|. (snd " ++ s ++ ") )"
+evalPrim (PrimBXor ty) s        = "((fst " ++ s ++ ") `xor` (snd " ++ s ++ "))"
+evalPrim (PrimBNot ty) s        = "(complement " ++ s ++ ")"
+evalPrim (PrimBShiftL ty) s     = "(shiftL (fst " ++ s ++ ") (snd " ++ s ++ "))"
+evalPrim (PrimBShiftR ty) s     = "(shiftR (fst " ++ s ++ ") (snd " ++ s ++ "))"
+evalPrim (PrimBRotateL ty) s    = "(rotateL (fst " ++ s ++ ") (snd " ++ s++ "))"
+evalPrim (PrimBRotateR ty) s    = "(rotateR (fst " ++ s ++ ") (snd " ++ s++ "))"
+evalPrim (PrimFDiv ty) s        = "((fst " ++ s ++ ") / (snd " ++ s ++ ") )"
+evalPrim (PrimRecip ty) s       = "(recip " ++ s ++ ")"
+evalPrim (PrimSin ty) s         = "(sin " ++ s ++ ")"
+evalPrim (PrimCos ty) s         = "(cos " ++ s ++ ")"
+evalPrim (PrimTan ty) s         = "(tan " ++ s ++ ")"
+evalPrim (PrimAsin ty) s        = "(asin " ++ s ++ ")"
+evalPrim (PrimAcos ty) s        = "(acos " ++ s ++ ")"
+evalPrim (PrimAtan ty) s        = "(atan " ++ s ++ ")"
+evalPrim (PrimAsinh ty) s       = "(asinh " ++ s ++ ")"
+evalPrim (PrimAcosh ty) s       = "(acosh " ++ s ++ ")"
+evalPrim (PrimAtanh ty) s       = "(atanh " ++ s ++ ")"
+evalPrim (PrimExpFloating ty) s = "(exp " ++ s ++ ")"
+evalPrim (PrimSqrt ty) s        = "(sqrt " ++ s ++ ")"
+evalPrim (PrimLog ty) s         = "(log " ++ s ++ ")"
+evalPrim (PrimFPow ty) s        = "( (fst " ++ s ++ ") ** (snd " ++ s ++ ") )"
+evalPrim (PrimLogBase ty) s     = "(logbase (fst " ++ s ++ ") (snd " ++ s ++"))"
+evalPrim (PrimTruncate ta tb) s = "(truncate " ++ s ++ ")"
+evalPrim (PrimRound ta tb) s    = "(round " ++ s ++ ")"
+evalPrim (PrimFloor ta tb) s    = "(floor " ++ s ++ ")"
+evalPrim (PrimCeiling ta tb) s  = "(ceiling " ++ s ++ ")"
+evalPrim (PrimAtan2 ty) s       = "(atan2 " ++ s ++ ")"
+evalPrim (PrimLt ty) s          = "( (fst " ++ s ++ ") < (snd " ++ s ++ ") )"
+evalPrim (PrimGt ty) s          = "( (fst " ++ s ++ ") > (snd " ++ s ++ ") )"
+evalPrim (PrimLtEq ty) s        = "( (fst " ++ s ++ ") <= (snd " ++ s ++ ") )"
+evalPrim (PrimGtEq ty) s        = "( (fst " ++ s ++ ") >= (snd " ++ s ++ ") )"
+evalPrim (PrimEq ty) s          = "( (fst " ++ s ++ ") == (snd " ++ s ++ ") )"
+evalPrim (PrimNEq ty) s         = "( (fst " ++ s ++ ") /= (snd " ++ s ++ ") )"
+evalPrim (PrimMax ty) s         = "((fst " ++ s ++ ") `max` (snd " ++ s ++ "))"
+evalPrim (PrimMin ty) s         = "((fst " ++ s ++ ") `min` (snd " ++ s ++ "))"
+evalPrim (PrimLAnd) s           = "( (fst " ++ s ++ ") && (snd " ++ s ++ ") )"
+evalPrim (PrimLOr) s            = "( (fst " ++ s ++ ") || (snd " ++ s ++ ") )"
+evalPrim (PrimLNot) s           = "(not " ++ s ++ ")"
+evalPrim (PrimOrd) s            = "(ord " ++ s ++ ")"
+evalPrim (PrimChr) s            = "(chr " ++ s ++ ")"
+evalPrim (PrimBoolToInt) s      = "(fromEnum " ++ s ++ ")"
+evalPrim (PrimFromIntegral ta tb) s = "(fromIntegral " ++ s ++ ")"
