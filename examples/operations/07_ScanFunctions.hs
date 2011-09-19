c8fc6dffec1fe3e89f5af30714555168ca5672ab
Ben Lambert-Smith <blambo+github@gmail.com>
2011-09-19 23:14:04 +1000

    Added scanl1 test

--- examples/operations/07_ScanFunctions.hs
+++ examples/operations/07_ScanFunctions.hs
@@ -8,6 +8,8 @@ main = do
    putStrLn $ Repa.run $ Acc.scanl (+) (constant (0 :: Int)) $ generate (index1 10) (\_ -> 1)
    --scanl'
    --scanl1
+   putStrLn "---SCANL1---"
+   putStrLn $ Repa.run $ Acc.scanl1 (+) $ generate (index1 10) (\_ -> constant (1 :: Int))
    --scanr
    --scanr'
    --scanr1
