2b3fad7517b90d1e61792ac8d496b807cd65ba2b
Ben Lambert <blambo+github@gmail.com>
2011-09-19 17:31:15 +1000

    Added proper printing of Repa file

--- Data/Array/Accelerate/Repa.hs
+++ Data/Array/Accelerate/Repa.hs
@@ -20,7 +20,25 @@ import qualified Data.Array.Accelerate.Smart as Smart
 import Data.Array.Accelerate.Repa.Evaluations (evalAcc)
 
 -- | Used to compile and run an embedded array program using the Repa backend
+--run :: Arrays a => Smart.Acc a -> String
+--run = evalAcc . Smart.convertAcc
+
 run :: Arrays a => Smart.Acc a -> String
-run = evalAcc . Smart.convertAcc
+run acc = headS ++ (evalAcc $ Smart.convertAcc acc) ++ tailS
 
+headS :: String
+{-# INLINE headS #-}
+headS =
+   "import Data.Array.Repa as Repa\n" ++
+   "main = putStrLn $ show $ toList $ "
 
+tailS :: String
+{-# INLINE tailS #-}
+tailS =
+   "\n" ++
+   "\n"
+   {-"\n" ++
+   "\n" ++
+   "size :: (Shape sh) => sh -> Int\n" ++
+   "size  Z     = 1\n" ++
+   "size (h:.t) = t * (Main.size h)\n"-}
