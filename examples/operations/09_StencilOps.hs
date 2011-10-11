142cdcc80017146bacfc335505ec8d8b1090d8cd
Ben Lambert <blambo+github@gmail.com>
2011-10-11 21:50:33 +1100

    Added a simple Stencil test

---
+++ examples/operations/09_StencilOps.hs
@@ -0,0 +1,19 @@
+import Data.Array.Accelerate as Acc
+import Data.Array.Accelerate.Repa as Repa
+import Data.Array.Accelerate.Interpreter as Interpreter
+
+main = do
+   -- stencil
+   putStrLn "---STENCIL---"
+   putStrLn $ Repa.run $ myStencil
+   putStrLn $ show $ Interpreter.run $ myStencil
+   -- stencil2
+
+myStencil :: Acc (Vector Int)
+myStencil = stencil myFun (Constant 0) $ theArray
+
+theArray :: Acc (Vector Int)
+theArray = generate (index1 10) (\_ -> constant 1)
+
+--myFun :: (Integral a) => (a, a, a) -> a
+myFun (a,b,c,d,e) = a + b + c + d + e
