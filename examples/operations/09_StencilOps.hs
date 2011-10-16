3b5f00c532c35c7ad7094c014bf83ad4f508b3d1
Ben Lambert <blambo+github@gmail.com>
2011-10-16 18:23:24 +1100

    Added a stencil test

--- examples/operations/09_StencilOps.hs
+++ examples/operations/09_StencilOps.hs
@@ -6,7 +6,8 @@ main = do
    -- stencil
    putStrLn "---STENCIL---"
    putStrLn $ Repa.run $ myStencil
-   putStrLn $ show $ Interpreter.run $ myStencil
+   --putStrLn $ show $ Interpreter.run $ myStencil
+   putStrLn $ show $ Repa.run $ myStencil
    -- stencil2
 
 myStencil :: Acc (Vector Int)
@@ -16,4 +17,4 @@ theArray :: Acc (Vector Int)
 theArray = generate (index1 10) (\_ -> constant 1)
 
 --myFun :: (Integral a) => (a, a, a) -> a
-myFun (a,b,c,d,e) = a + b + c + d + e
+myFun (a,b,c) = a + b + c
