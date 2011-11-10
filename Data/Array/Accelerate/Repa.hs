e8656f310967a4a57ee42d6eb827a7d5bcf1ba22
Ben Lambert-Smith <blambo+github@gmail.com>
2011-11-10 16:13:52 +1100

    Wibble

--- Data/Array/Accelerate/Repa.hs
+++ Data/Array/Accelerate/Repa.hs
@@ -38,7 +38,7 @@ headS :: Doc
 headS =
    text "{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeOperators #-}" $+$
    text "{-# LANGUAGE FlexibleContexts #-}" $+$
-   text "module Acc-Repa where" $+$
+   text "module AccRepa where" $+$
    text "import Data.Array.Repa as Repa" $+$
    text "import Data.Bits -- required for Prim ops" $+$
    text "import Data.Char -- required for Prim ops" $+$
