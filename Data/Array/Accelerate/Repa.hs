946737f055739cd5adcfd3c283d9d9293aaea371
Ben Lambert-Smith <blambo+github@gmail.com>
2011-11-17 15:53:33 +1100

    Cleaned up Repa.hs
    * Changed a few names of functions
    * 'run' will now compile and execute code, outputting a Repa array
    * still some bugs lurking about the place, but is vaguely working
    * Added use of directory library for removing temporary compile file

--- Data/Array/Accelerate/Repa.hs
+++ Data/Array/Accelerate/Repa.hs
@@ -10,60 +10,103 @@
 
 module Data.Array.Accelerate.Repa
    ( Arrays
+   , accToRepa
+   , compile
+   , exec
    , run
-   , compileToFile
    )
    where
 
-import Data.Array.Accelerate.AST
+import Data.Array.Accelerate
 import qualified Data.Array.Accelerate.Smart as Smart
-
 import Data.Array.Accelerate.Repa.Evaluations (evalAcc)
 import Data.Array.Accelerate.Repa.Stencil (stencilDoc)
 
 import Text.PrettyPrint
 
-import GHC       -- For compiling and running using GHC API
-import GHC.Paths (libdir) -- simplifies use of GHC API
-import System.IO -- For writing source to a file
+import qualified Data.Array.Repa as Repa
 
--- | Used to compile and run an embedded array program using the Repa backend
-run :: Arrays a => Smart.Acc a -> String
-run acc = show $
-   headS $$ (nest 1 (evalAcc $ Smart.convertAcc acc))
-         $$ tailS
-         $+$ text " "
-         $$ stencilDoc
+import GHC
+import GHC.Paths (libdir)
+import DynFlags
 
-compileToFile :: Arrays a => Maybe String -> Smart.Acc a -> IO ()
-compileToFile targetFile acc = do
-   let f = case targetFile of
-              Just s -> s
-              Nothing -> defaultFile
-   -- writes source to file as currently can't compile from String
-   writeFile f $ run acc
-   -- using GHC API from here
-   r <- loadAndCompile f
-   case r of
-      Just err -> error err
-      Nothing  -> return ()
+import Unsafe.Coerce
+import System.IO
+import System.Directory (removeFile)
 
-loadAndCompile :: String -> IO (Maybe String)
-loadAndCompile targetFile = runGhc (Just libdir) $ do
+-- | Using the provided Accelerate program, run will compile, execute
+-- and return the result of a given Accelerate program using Repa for
+-- execution
+run :: (Arrays a, Repa.Shape sh, Repa.Elt e)
+    => Smart.Acc a -- ^ The Accelerate program
+    -> IO (Repa.Array sh e)
+run acc = do
+   -- Generate source code from Accelerate program
+   let src = accToRepa acc
+   -- Write source code to temporary file in /tmp
+   (name, handle) <- openTempFile tempDir fileName
+   hPutStr handle src
+   hClose handle
+
+   -- Perform GHC API operations
+   result <- runGhc (Just libdir) $ do
+      -- compile
+      err <- compile name
+      -- execute compiled, checking for error
+      case err of
+         Just errStr -> error errStr
+         Nothing     -> exec modName fnName
+
+   -- Delete temporary file
+   removeFile name
+
+   -- Return result of Accelerate program
+   return result
+
+-- | Compiles the given file name
+compile :: String -- ^ The source file
+        -> Ghc (Maybe String)
+compile path = do
    dflags <- getSessionDynFlags
    setSessionDynFlags (dflags{
-      optLevel = 2
+      optLevel = 2,
+      ghcLink = LinkInMemory,
+      hscTarget = HscInterpreted
       })
-   target <- guessTarget targetFile Nothing
+   target <- guessTarget path Nothing
    addTarget target
    r <- load LoadAllTargets
    return $ case r of
       Failed    -> Just "Error in module loading"
       Succeeded -> Nothing
 
-headS :: Doc
-{-# INLINE headS #-}
-headS =
+-- | Executes the given function in the given module, must already be
+-- compiled and loaded
+exec :: (Repa.Shape sh, Repa.Elt e)
+     => String -- ^ The module name
+     -> String -- ^ The function name
+     -> Ghc (Repa.Array sh e)
+exec modName fnName = do
+   mod <- findModule (mkModuleName modName) Nothing
+   setContext [mod] []
+   value <- compileExpr (modName Prelude.++ "." Prelude.++ fnName)
+
+   let value' = (unsafeCoerce value) :: Repa.Array sh e
+   return value'
+
+-- | Converts an Accelerate program to a Repa program and returns the
+-- source as a String
+accToRepa :: (Arrays a)
+          => Smart.Acc a -- ^ The Accelerate program
+          -> String
+accToRepa acc = show $
+   headD $$ (nest 1 (evalAcc (Smart.convertAcc acc)))
+         $$ tailD
+         $$ stencilDoc
+
+headD :: Doc
+{-# INLINE headD #-}
+headD =
    text "{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeOperators #-}" $+$
    text "{-# LANGUAGE FlexibleContexts #-}" $+$
    text "module" <+> text modName <+> text "where" $+$
@@ -74,16 +117,25 @@ headS =
    text "import Data.List (sortBy)  -- required for permute" $+$
    text "import Data.Ord  (compare) -- required for permute" $+$
    text " " $+$
-   text "main = putStrLn $ show $"
+   text "main = putStrLn $ show $" <+> text fnName $+$
+   text fnName <+> equals
 
-tailS :: Doc
-{-# INLINE tailS #-}
-tailS = empty
+tailD :: Doc
+{-# INLINE tailD #-}
+tailD = empty
 
 modName :: String
 {-# INLINE modName #-}
-modName = "Main"
+modName = "RepaTest"
 
-defaultFile :: String
-{-# INLINE defaultFile #-}
-defaultFile = "AccRepa.hs"
+fnName :: String
+{-# INLINE fnName #-}
+fnName = "repa"
+
+fileName :: String
+{-# INLINE fileName #-}
+fileName = modName Prelude.++ ".hs"
+
+tempDir :: FilePath
+{-# INLINE tempDir #-}
+tempDir = "/tmp/"
